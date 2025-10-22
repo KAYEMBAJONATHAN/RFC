e2c0db0891112600705b9ff4d93af4520c55ed4c
jonathan <jkayemba@retrorabbit.co.za>
2025-10-22 09:07:28 +0200

    Add the pop up the menu on the nav

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +11,28 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.css'
 })
 export class NavbarComponent {
+  popupOpen: boolean = true;
+  menuOpen: boolean = false;
 
+  constructor(private router: Router) { }
+
+  toggleMenu(): void {
+    if (!this.popupOpen) {
+      this.menuOpen = !this.menuOpen;
+    }
+  }
+
+  redirectToLogin(): void {
+    this.popupOpen = false;
+    this.router.navigate(['/login']);
+  }
+
+  redirectToSignup(): void {
+    this.popupOpen = false;
+    this.router.navigate(['/signup']);
+  }
+
+  closePopup(): void {
+    this.popupOpen = false;
+  }
 }
